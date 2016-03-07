236df6ef7ef85df00deffefdab288ef28aa8654f
hadasbenisty <hadas.benisty@gmail.com>
2016-03-07 10:36:10 +0200

    New files for utils

--- plotByClustering.m
+++ plotByClustering.m
@@ -1,22 +1,29 @@
-function plotByClustering(meanMat, allMat, ttl, timeFrames)
-
-if nargin == 3
-    timeFrames = 1:size(meanMat, 2);
+function plotByClustering(meanMat, ttl, timeFrames)
+if iscell(meanMat)
+    figure;
+if nargin == 2
+    timeFrames = 1:size(meanMat{1}, 2);
 end
-figure;
-subplot(1,2,1);
-imagesc(meanMat(:, timeFrames));
-set(gca, 'Ytick', 1:size(meanMat,2));title(sprintf('%s\nMean Image', ttl));
-for k=1:length(allMat)
-   subplot(length(allMat), 2, 2*k);
-   imagesc(allMat{k}(:,timeFrames));
+for k=1:length(meanMat)
+   subplot(length(meanMat), 1, k);
+%    subplot(length(allMat), 2, 2*k);
+   imagesc(meanMat{k}(:,timeFrames));
    set(gca, 'XtickLabel',{});
    set(gca, 'YtickLabel',{});
    colorbar;
    ylabel(num2str(k));
    if k == 1
-       title('Ordered Data');
+       title(ttl);
    end
+   
+end
+else
+if nargin == 2
+    timeFrames = 1:size(meanMat, 2);
+end
+figure;
+imagesc(meanMat(:, timeFrames));
+set(gca, 'Ytick', 1:size(meanMat,2));title(sprintf('%s\nMean Image', ttl));
 end
 % figure;
 % for k=1:size(meanMat,1)
